41839d905fa05d6cfe320eb2eb3da739ccfe665f
Shripathi Raman <shripathiram@gmail.com>
2021-01-31 13:16:29 +0530

    Test

--- al_gs_connect.py
+++ al_gs_connect.py
@@ -6,3 +6,5 @@ gc = gspread.service_account(filename=prm.al_cred_file)
 sh = gc.open_by_key(prm.al_driving_gs_file_id)
 ws = sh.worksheet(prm.al_driving_gs_sheet_name)
 res = ws.get_all_values()
+
+print();
